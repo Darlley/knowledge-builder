d0653c44baa876849401d87b5ca99e46ffa06af2
Darlley <systembrtt@gmail.com>
2024-08-24 14:45:22 -0400

    feat: zoo actions

---
+++ src/app/actions.ts
@@ -0,0 +1,14 @@
+'use server'
+
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server"
+import { redirect } from "next/navigation"
+
+ 
+export async function createSiteAction() {
+  const {getUser} = getKindeServerSession()
+  const user = await getUser()
+
+  if(!user){
+    return redirect('/api/auth/login')
+  }
+}
