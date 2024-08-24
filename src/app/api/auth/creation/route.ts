47c2f058822c05eda879f99f6e3a9ebb14ed7cc6
Darlley <systembrtt@gmail.com>
2024-08-23 21:24:24 -0400

    sync king auth with db user

---
+++ src/app/api/auth/creation/route.ts
@@ -0,0 +1,30 @@
+import prisma from "@/utils/db";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { NextResponse } from "next/server";
+
+export async function GET(){
+  const { getUser } = getKindeServerSession()
+  const user = await getUser()
+  if(!user || user === null || !user?.id) throw new Error('Algo errado aconteceu...')
+
+  let dbUser = await prisma.user.findUnique({
+    where: {
+      id: user.id
+    }
+  })
+
+  if(!dbUser){
+    dbUser = await prisma.user.create({
+      data: {
+        id: user?.id ?? '',
+        firstName: user?.given_name ?? '',
+        lastName: user?.family_name ?? '',
+        name: user?.username ?? '',
+        email: user?.email ?? '',
+        profileImage: user?.picture ?? `https://avatar.vercel.sh/${user?.given_name}`
+      }
+    })
+  }
+
+  return NextResponse.redirect(`${process.env.KINDE_SITE_URL!}/dashboard`)
+}
