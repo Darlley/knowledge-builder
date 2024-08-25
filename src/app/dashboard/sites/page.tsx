27c91118c2151ac2e86a5df13d1608107a1bf210
Darlley <systembrtt@gmail.com>
2024-08-25 17:43:30 -0400

    list sites

--- src/app/dashboard/sites/page.tsx
+++ src/app/dashboard/sites/page.tsx
@@ -1,8 +1,16 @@
 'use client';
 
 import { useKindeBrowserClient } from '@kinde-oss/kinde-auth-nextjs';
-import { Button, Image, Spinner } from '@nextui-org/react';
-import clsx from 'clsx';
+import {
+  Button,
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  Image,
+  Spinner,
+  User,
+} from '@nextui-org/react';
 import { File, Plus } from 'lucide-react';
 import moment from 'moment';
 import 'moment/locale/pt-br';
@@ -65,45 +73,50 @@ export default function page() {
           </Button>
         </div>
       ) : (
-        sites.map((site) => (
-          <div className="max-w-xs w-full group/card" key={site?.id}>
-            <div
-              className={clsx(
-                'cursor-pointer overflow-hidden relative card h-96 rounded-md shadow-xl  max-w-sm mx-auto backgroundImage flex flex-col justify-between p-4 bg-cover bg-[url(https://images.unsplash.com/photo-1544077960-604201fe74bc?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1651&q=80)]'
-              )}
-              // style={{
-              //   backgroundImage: 'https://images.unsplash.com/photo-1544077960-604201fe74bc?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1651&q=80',
-              // }}
-            >
-              <div className="absolute w-full h-full top-0 left-0 transition duration-300 group-hover/card:bg-black opacity-60"></div>
-              <div className="flex flex-row items-center space-x-4 z-10">
-                <Image
-                  height="100"
-                  width="100"
-                  alt="Avatar"
-                  src={user?.picture ?? ''}
-                  className="h-10 w-10 rounded-full border-2 object-cover"
-                />
-                <div className="flex flex-col">
-                  <p className="font-normal text-base text-gray-50 relative z-10">
-                    {user?.given_name}
-                  </p>
-                  <p className="text-sm text-gray-400">
-                    {moment(site?.createdAt).format('DD [de] MMMM [de] YYYY')}
-                  </p>
-                </div>
-              </div>
-              <div className="text content">
-                <h1 className="font-bold text-xl md:text-2xl text-gray-50 relative z-10">
-                  {site?.name}
-                </h1>
-                <p className="font-normal text-sm text-gray-50 relative z-10 my-4">
-                  {site?.description}
-                </p>
-              </div>
-            </div>
-          </div>
-        ))
+        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
+          {sites.map((site) => (
+            <Link key={site?.id} href={`/dashboard/sites/${site?.id}`}>
+              <Card
+                
+                classNames={{
+                  base: 'bg-gray-950 border border-gray-900',
+                  footer: 'flex w-full justify-between items-center'
+                }}
+              >
+                <CardHeader>
+                  <Image
+                    src="/default.png"
+                    fallbackSrc="/default.png"
+                    alt="NextUI Image with fallback"
+                  />
+                </CardHeader>
+                <CardBody>
+                  <div>
+                    <h3 className='text-lg font-bold'>
+                      {site.name}
+                    </h3>
+                    <p>{site.description}</p>
+                  </div>
+                </CardBody>
+                <CardFooter>
+                  <div>
+                    <User
+                      name={user?.given_name}
+                      avatarProps={{
+                        src: user?.picture!,
+                      }}
+                    />
+                  </div>
+                  <div>
+                    <p className='text-xs'>
+                      {moment(site.createdAt).format('DD [de] MMMM [de] YYYY')}
+                    </p>
+                  </div>
+                </CardFooter>
+              </Card>
+            </Link>
+          ))}
+        </div>
       )}
     </>
   );
