c2d9bb1680c9c892555c16fb97825f543094523b
Darlley <systembrtt@gmail.com>
2024-09-12 22:14:33 -0400

    feat: formulário editar site

--- src/app/dashboard/sites/[id]/settings/page.tsx
+++ src/app/dashboard/sites/[id]/settings/page.tsx
@@ -1,15 +1,21 @@
 'use client';
 
 import UploadImageForm from '@/components/UploadImageForm';
+import { siteSchema, SiteSchema } from '@/utils/zodSchemas';
+import { zodResolver } from '@hookform/resolvers/zod';
 import {
   Button,
   Card,
   CardBody,
   CardFooter,
   CardHeader,
+  Input,
+  Textarea,
 } from '@nextui-org/react';
-import { ChevronLeft, Cog, Plus } from 'lucide-react';
+import { ChevronLeft, Cog, Plus, Save } from 'lucide-react';
 import Link from 'next/link';
+import { Controller, SubmitHandler, useForm } from 'react-hook-form';
+import { toast } from 'sonner';
 
 export default function page({
   params,
@@ -19,6 +25,22 @@ export default function page({
   };
 }) {
   const { id: siteId } = params;
+
+  // Adicione o hook useForm e a função onSubmit
+  const {
+    control,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<SiteSchema>({
+    resolver: zodResolver(siteSchema),
+    mode: 'onBlur',
+  });
+
+  const onSubmit: SubmitHandler<SiteSchema> = async (data) => {
+    // Implemente a lógica de atualização do site aqui
+    console.log(data);
+    toast.success('Site atualizado com sucesso');
+  };
 
   return (
     <div className="flex flex-1 flex-col gap-4 lg:gap-6 p-4 lg:p-6">
@@ -35,20 +57,77 @@ export default function page({
           </Button>
           <h1 className="text-xl">Configurações</h1>
         </div>
+      </div>
 
-        <div className="gap-2 flex items-center">
-          <Button endContent={<Cog className="stroke-[1.5] size-5" />}>
-            <span>Configurar</span>
-          </Button>
-          <Button endContent={<Plus />} color="primary">
-            <span>Criar artigo</span>
-          </Button>
+      <div className="w-full flex gap-4 flex-col md:flex-row">
+        <Card className="w-6/12 p-4 shadow-none dark:bg-gray-950 border dark:border-gray-900">
+          <CardHeader>
+            <h3 className="text-lg font-semibold">Editar informações</h3>
+          </CardHeader>
+          <CardBody>
+            <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-4">
+              <Controller
+                name="name"
+                control={control}
+                defaultValue=""
+                render={({ field }) => (
+                  <Input
+                    {...field}
+                    label="Nome do site"
+                    isRequired
+                    isInvalid={!!errors.name}
+                    errorMessage={errors.name?.message}
+                  />
+                )}
+              />
+              <Controller
+                name="subdirectory"
+                control={control}
+                defaultValue=""
+                render={({ field }) => (
+                  <Input
+                    {...field}
+                    label="Subdiretório"
+                    isRequired
+                    isInvalid={!!errors.subdirectory}
+                    errorMessage={errors.subdirectory?.message}
+                  />
+                )}
+              />
+              <Controller
+                name="description"
+                control={control}
+                defaultValue=""
+                render={({ field }) => (
+                  <Textarea
+                    {...field}
+                    label="Descrição"
+                    isRequired
+                    isInvalid={!!errors.description}
+                    errorMessage={errors.description?.message}
+                  />
+                )}
+              />
+            </form>
+          </CardBody>
+          <CardFooter>
+            <Button
+              color="primary"
+              type="submit"
+              isLoading={isSubmitting}
+              endContent={<Save className="stroke-[1.5] size-5" />}
+            >
+              Salvar alterações
+            </Button>
+          </CardFooter>
+        </Card>
+
+        <div className="w-6/12">
+          <UploadImageForm onImageUpload={console.log} />
         </div>
       </div>
 
       <div className="w-full flex gap-4 flex-col">
-        <UploadImageForm onImageUpload={console.log} />
-
         <Card
           classNames={{
             base: 'bg-danger-100 dark:bg-danger-500/10 border border-danger p-4',
--- src/components/UploadImageForm/UploadImageForm.tsx
+++ src/components/UploadImageForm/UploadImageForm.tsx
@@ -1,13 +1,22 @@
 'use client';
 
-import { useState } from 'react';
-import { Button, Card, CardBody, CardFooter, CardHeader, Image } from '@nextui-org/react';
 import { UploadDropzone } from '@/utils/uploadthing';
-import { Check, X } from 'lucide-react';
+import {
+  Button,
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  Image,
+} from '@nextui-org/react';
+import { Check, Save, X } from 'lucide-react';
+import { useState } from 'react';
 import { toast } from 'sonner';
 import { UploadImageFormProps } from './UploadImageForm.types';
 
-export default function UploadImageForm({ onImageUpload }: UploadImageFormProps) {
+export default function UploadImageForm({
+  onImageUpload,
+}: UploadImageFormProps) {
   const [imageUrl, setImageUrl] = useState<string | null>(null);
 
   const handleUploadComplete = (res: { url: string }[]) => {
@@ -25,9 +34,9 @@ export default function UploadImageForm({ onImageUpload }: UploadImageFormProps)
   };
 
   return (
-    <Card className="w-full p-4 shadow-none dark:bg-gray-950 border dark:border-gray-900">
+    <Card className="w-full h-full p-4 shadow-none dark:bg-gray-950 border dark:border-gray-900">
       <CardHeader>
-        <h3 className="text-lg font-semibold">Imagem do Site</h3>
+        <h3 className="text-lg font-semibold">Mudar imagem</h3>
       </CardHeader>
       <CardBody>
         {imageUrl ? (
@@ -37,26 +46,15 @@ export default function UploadImageForm({ onImageUpload }: UploadImageFormProps)
               alt="Imagem do site"
               className="w-full size-[200px] object-cover rounded-lg"
             />
-            <div className="absolute top-2 right-2 z-10 flex gap-2">
-              <Button
-                isIconOnly
-                size="sm"
-                color="primary"
-                variant='shadow'
-                onClick={() => toast.success('Imagem do site atualizada! 🎉')}
-              >
-                <Check className="w-4 h-4 stroke-white" />
-              </Button>
               <Button
                 isIconOnly
                 size="sm"
                 color="danger"
+                className="absolute top-2 right-2 z-10 flex gap-2"
                 onClick={handleRemoveImage}
               >
                 <X className="w-4 h-4" />
               </Button>
-
-            </div>
           </div>
         ) : (
           <UploadDropzone
@@ -65,14 +63,24 @@ export default function UploadImageForm({ onImageUpload }: UploadImageFormProps)
             onUploadError={handleUploadError}
             appearance={{
               button: 'bg-primary text-white',
-              container: 'border-2 border-dashed border-gray-300 dark:border-gray-700',
+              container:
+                'border-2 border-dashed border-gray-300 dark:border-gray-700',
               label: 'text-sm text-gray-500 dark:text-gray-400',
             }}
           />
         )}
       </CardBody>
-      <CardFooter>
-        <p className="text-xs text-gray-500">
+      <CardFooter className="flex justify-between items-center gap-2">
+        <div>
+          <Button
+            color="primary"
+            type="submit"
+            endContent={<Save className="stroke-[1.5] size-5" />}
+          >
+            Salvar alterações
+          </Button>
+        </div>
+        <p className="text-xs text-gray-500 text-right w-full">
           Faça upload de uma imagem para representar o seu site.
         </p>
       </CardFooter>
