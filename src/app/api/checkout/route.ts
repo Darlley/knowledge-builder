60df580290370fcde00c0df7cd585078eed159ac
Darlley <systembrtt@gmail.com>
2024-09-21 18:37:24 -0400

    feat: payment link

--- src/app/api/checkout/route.ts
+++ src/app/api/checkout/route.ts
@@ -1,76 +1,86 @@
-import { NextRequest, NextResponse } from 'next/server';
-import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
 import prisma from '@/utils/db';
 import { requireUser } from '@/utils/requireUser';
 import { stripe } from '@/utils/stripe';
-import { redirect } from 'next/navigation';
+import { NextRequest, NextResponse } from 'next/server';
 
 export async function POST(request: NextRequest) {
-  const { userId } = await request.json();
+  const { userId, planId } = await request.json();
+  console.log({ userId, planId });
   const user = await requireUser();
 
-  if (!user) {
-    return NextResponse.redirect('/api/auth/login');
-  }
-
-  try {
-    let stripeUserId = await prisma.user.findUnique({
-      where: {
-        id: userId,
-      },
-      select: {
-        stripeCustomerId: true,
-        email: true,
-        firstName: true,
-      },
-    });
-
-    if(!stripeUserId?.stripeCustomerId){
-      const customer = await stripe.customers.create({
-        email: stripeUserId?.email,
-        name: stripeUserId?.firstName,
-      });
-
-      stripeUserId = await prisma.user.update({
+  if (userId && planId) {
+    try {
+      // Use o ID do usuário autenticado em vez do userId do corpo da requisição
+      let stripeUserId = await prisma.user.findUnique({
         where: {
-          id: userId,
+          id: userId, // Use o ID do usuário autenticado
         },
-        data: {
-          stripeCustomerId: customer?.id,
+        select: {
+          stripeCustomerId: true,
+          email: true,
+          firstName: true,
         },
       });
-    }
 
-    // Criar uma sessão de checkout do Stripe
-    const session = await stripe.checkout.sessions.create({
-      customer: stripeUserId?.stripeCustomerId as string,
-      mode: 'subscription', // Modo de assinatura
-      billing_address_collection: 'auto',
-      payment_method_types: ['card', 'pix'], // Aceita pagamentos com cartão
-      customer_update: {
-        address: 'auto',
-        name: 'auto',
-      },
-      success_url: 'http://localhost:3000/dashboard/payment/success', // URL de redirecionamento em caso de sucesso
-      cancel_url: 'http://localhost:3000/dashboard/payment/cancelled', // URL de redirecionamento em caso de cancelamento
-      line_items: [
-        {
-          price: process.env.STRIPE_STARTUP_PRICE_ID, // ID do preço do plano Pro
-          quantity: 1, // Quantidade do item (assinatura)
+      if (!stripeUserId?.stripeCustomerId) {
+        const customer = await stripe.customers.create({
+          email: stripeUserId?.email,
+          name: stripeUserId?.firstName,
+        });
+
+        stripeUserId = await prisma.user.update({
+          where: {
+            id: userId, // Use o ID do usuário autenticado
+          },
+          data: {
+            stripeCustomerId: customer?.id,
+          },
+        });
+      }
+
+      let priceId;
+      switch (planId) {
+        case 'plan-startup':
+          priceId = process.env.STRIPE_STARTUP_PRICE_ID;
+          break;
+        case 'plan-enterprise':
+          priceId = process.env.STRIPE_ENTERPRISE_PRICE_ID;
+          break;
+        default:
+          throw new Error('Plano inválido');
+      }
+
+      const session = await stripe.checkout.sessions.create({
+        customer: stripeUserId?.stripeCustomerId as string,
+        mode: 'subscription',
+        billing_address_collection: 'auto',
+        payment_method_types: ['card'],
+        customer_update: {
+          address: 'auto',
+          name: 'auto',
         },
-        {
-          price: process.env.STRIPE_ENTERPRISE_PRICE_ID, // ID do preço do plano Pro
-          quantity: 1, // Quantidade do item (assinatura)
-        }
-      ],
-    });
+        success_url: 'http://localhost:3000/dashboard/payment/success',
+        cancel_url: 'http://localhost:3000/dashboard/payment/cancelled',
+        line_items: [
+          {
+            price: priceId,
+            quantity: 1,
+          },
+        ],
+      });
 
-    return redirect(session.url as string);
-  } catch (error) {
-    console.error('Erro ao criar sessão de checkout:', error);
-    return NextResponse.json(
-      { error: 'Erro ao processar o checkout' },
-      { status: 500 }
-    );
+      return NextResponse.json({ url: session.url });
+    } catch (error) {
+      console.error('Erro ao criar sessão de checkout:', error);
+      return NextResponse.json(
+        { error: 'Erro ao processar o checkout' },
+        { status: 500 }
+      );
+    }
   }
+
+  return NextResponse.json(
+    { error: 'Erro ao processar o checkout' },
+    { status: 500 }
+  );
 }
--- src/components/Pricing/Pricing.tsx
+++ src/components/Pricing/Pricing.tsx
@@ -1,19 +1,29 @@
-'use client'
+'use client';
 
-import { useState } from 'react'
-import { Radio, RadioGroup, Card, CardBody, CardHeader, Button, Chip } from '@nextui-org/react'
-import { CheckCircle, CheckIcon } from 'lucide-react'
+import { useKindeBrowserClient } from '@kinde-oss/kinde-auth-nextjs';
+import {
+  Button,
+  Card,
+  CardBody,
+  CardHeader,
+  Chip,
+  Radio,
+  RadioGroup,
+} from '@nextui-org/react';
+import { CheckCircle } from 'lucide-react';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
 
 const frequencies = [
   { value: 'monthly', label: 'Mensal', priceSuffix: '/mês' },
   { value: 'yearly', label: 'Anual', priceSuffix: '/ano' },
-]
+];
 
 const plans = [
   {
     name: 'Freelancer',
     id: 'plan-freelancer',
-    href: '#',
+    href: '/dashboard',
     price: { monthly: 'Grátis', yearly: 'Grátis' },
     description: 'Perfeito para começar seu blog.',
     features: [
@@ -71,29 +81,73 @@ const plans = [
     ],
     mostPopular: false,
   },
-]
+];
 
 export default function Pricing() {
-  const [frequency, setFrequency] = useState(frequencies[0])
+  const [frequency, setFrequency] = useState(frequencies[0]);
+  const [loadingPlan, setLoadingPlan] = useState<string | null>(null);
+  const router = useRouter();
+  const { user } = useKindeBrowserClient();
+
+  const handlePlanSelection = async (plan: (typeof plans)[0]) => {
+    setLoadingPlan(plan.id);
+    if (plan.name === 'Freelancer') {
+      router.push('/dashboard');
+    } else {
+      try {
+        const response = await fetch('/api/checkout', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            userId: user?.id,
+            planId: plan?.id,
+          }),
+        });
+
+        if (response.ok) {
+          const { url } = await response.json();
+          router.push(url);
+        } else {
+          console.error('Erro ao processar o checkout');
+        }
+      } catch (error) {
+        console.error('Erro ao fazer a requisição:', error);
+      }
+    }
+    setLoadingPlan(null);
+  };
 
   return (
     <div className="dark:bg-gray-950 p-4">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto max-w-4xl text-center">
-          <Chip color='primary' variant='shadow' className="text-xs font-semibold leading-7">Preços</Chip>
+          <Chip
+            color="primary"
+            variant="shadow"
+            className="text-xs font-semibold leading-7"
+          >
+            Preços
+          </Chip>
           <h1 className="mt-6 text-4xl font-bold tracking-tight text-gray-900 dark:text-white sm:text-5xl">
             Planos para todos os tipos de blogs
           </h1>
         </div>
         <p className="mx-auto mt-4 max-w-2xl text-center text-lg leading-8 text-gray-600 dark:text-gray-300">
-          Escolha o plano ideal para o seu blog, desde iniciantes até grandes operações.
+          Escolha o plano ideal para o seu blog, desde iniciantes até grandes
+          operações.
         </p>
         <div className="mt-16 flex justify-center">
           <RadioGroup
             label="Frequência de pagamento"
             orientation="horizontal"
             value={frequency.value}
-            onValueChange={(value) => setFrequency(frequencies.find((f) => f.value === value) || frequencies[0])}
+            onValueChange={(value) =>
+              setFrequency(
+                frequencies.find((f) => f.value === value) || frequencies[0]
+              )
+            }
           >
             {frequencies.map((option) => (
               <Radio key={option.value} value={option.value}>
@@ -114,9 +168,13 @@ export default function Pricing() {
               shadow="sm"
             >
               <CardHeader className="flex items-center justify-between">
-                <h3 className={`text-lg font-semibold ${
-                  plan.mostPopular ? 'text-primary' : 'text-gray-900 dark:text-white'
-                }`}>
+                <h3
+                  className={`text-lg font-semibold ${
+                    plan.mostPopular
+                      ? 'text-primary'
+                      : 'text-gray-900 dark:text-white'
+                  }`}
+                >
                   {plan.name}
                 </h3>
                 {plan.mostPopular && (
@@ -126,12 +184,16 @@ export default function Pricing() {
                 )}
               </CardHeader>
               <CardBody>
-                <p className="text-sm text-gray-600 dark:text-gray-400">{plan.description}</p>
+                <p className="text-sm text-gray-600 dark:text-gray-400">
+                  {plan.description}
+                </p>
                 <p className="mt-6 flex items-baseline gap-x-1">
                   <span className="text-4xl font-bold text-gray-900 dark:text-white">
                     {plan.price[frequency.value as keyof typeof plan.price]}
                   </span>
-                  <span className="text-sm font-semibold text-gray-600 dark:text-gray-400">{frequency.priceSuffix}</span>
+                  <span className="text-sm font-semibold text-gray-600 dark:text-gray-400">
+                    {frequency.priceSuffix}
+                  </span>
                 </p>
                 <ul className="my-8 space-y-3 text-sm text-gray-600 dark:text-gray-400">
                   {plan.features.map((feature) => (
@@ -142,12 +204,13 @@ export default function Pricing() {
                   ))}
                 </ul>
                 <Button
-                  href={plan.href}
+                  onClick={() => handlePlanSelection(plan)}
                   color={plan.mostPopular ? 'primary' : 'default'}
                   variant={plan.mostPopular ? 'solid' : 'flat'}
                   className="w-full mt-auto"
+                  isLoading={loadingPlan === plan.id}
                 >
-                  Comprar plano
+                  {loadingPlan === plan.id ? 'Processando...' : 'Comprar plano'}
                 </Button>
               </CardBody>
             </Card>
@@ -155,5 +218,5 @@ export default function Pricing() {
         </div>
       </div>
     </div>
-  )
+  );
 }
