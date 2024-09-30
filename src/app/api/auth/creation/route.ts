0ec0e503cd79f0b43bac35203bbfee046cc8ad03
Darlley <systembrtt@gmail.com>
2024-09-29 20:57:37 -0400

    fix: create customer

--- src/app/api/auth/creation/route.ts
+++ src/app/api/auth/creation/route.ts
@@ -1,4 +1,5 @@
 import prisma from "@/utils/db";
+import { stripe } from "@/utils/stripe";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { NextResponse } from "next/server";
 
@@ -14,6 +15,7 @@ export async function GET(){
   })
 
   if(!dbUser){
+    // Criar o usuário no banco de dados
     dbUser = await prisma.user.create({
       data: {
         id: user?.id ?? '',
@@ -23,7 +25,31 @@ export async function GET(){
         email: user?.email ?? '',
         profileImage: user?.picture ?? `https://avatar.vercel.sh/${user?.given_name}`
       }
-    })
+    });
+
+    // Criar cliente no Stripe usando os dados do novo dbUser
+    const customer = await stripe.customers.create({
+      email: dbUser.email,
+      name: `${dbUser.firstName} ${dbUser.lastName}`,
+    });
+
+    // Atualizar o dbUser com o ID do cliente Stripe
+    dbUser = await prisma.user.update({
+      where: { id: dbUser.id },
+      data: { stripeCustomerId: customer.id },
+    });
+  } else {
+    // Se o usuário já existir, buscar o cliente no Stripe
+    let customer = (await stripe.customers.list({
+      email: dbUser.email
+    })).data[0];
+
+    if (!customer) {
+      customer = await stripe.customers.create({
+        email: dbUser.email,
+        name: `${dbUser.firstName} ${dbUser.lastName}`,
+      });
+    }
   }
 
   return NextResponse.redirect(`${process.env.KINDE_SITE_URL!}/dashboard`)
